9cf0bef695df43b06f35dc23f676b79010a5ca27
L.L <lu18958121319@sina.com>
2023-09-10 23:39:52 +0800

    feat: 新增全局消息提示

--- blog-cms/src/plugins/index.ts
+++ blog-cms/src/plugins/index.ts
@@ -1,11 +1,17 @@
 import type { App } from "vue";
 // element plus
-import ElementPlus from "element-plus";
+import ElementPlus, { ElMessage } from "element-plus";
 import "element-plus/dist/index.css";
 import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 import "@/assets/css/element.scss";
 
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+
+declare module "vue" {
+  export interface ComponentCustomProperties {
+    $message: typeof ElMessage;
+  }
+}
 
 export default {
   install(app: App) {
@@ -15,5 +21,6 @@ export default {
     for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
       app.component(key, component);
     }
+    app.config.globalProperties.$message = ElMessage;
   },
 };
