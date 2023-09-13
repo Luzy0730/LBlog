e89c981073f889136076f5b8cb8e10a70156fa39
L.L <lu18958121319@sina.com>
2023-09-14 00:43:24 +0800

    upgrade: 封装mysql包 支持promise

--- blog-server/mysql/index.js
+++ blog-server/mysql/index.js
@@ -1,36 +1,54 @@
 const mysql = require("mysql");
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Lzy@960730",
-  database: "blog",
-  port: 3306,
-});
+const promisify = require("util").promisify;
 
-connection.connect();
-module.exports = (sql, params, cb) => {
-  connection.query(sql, params, function (error, results, fields) {
+let connection = null;
+
+const connect = () => {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Lzy@960730",
+    database: "blog",
+    port: 3306,
+  });
+  connection.connect((error) => {
     if (error) throw error;
-    let data = null;
-    switch (sql.split(" ")[0]) {
-      case "SELECT":
-        data = results;
-        break;
-      case "INSERT":
-        data = results.insertId;
-        break;
-      case "UPDATE":
-        data = null;
-        break;
-      default:
-        data = null;
-        break;
-    }
-    cb({
-      code: 200,
-      data,
-      msg: "success",
-    });
   });
 };
-// connection.end();
+
+const end = () => connection.end();
+
+module.exports = {
+  /**
+   *
+   * @param {string} sql sql语句
+   * @param {Array<any>} params sql占位参数
+   * @param {Boolean} auto 自动链接
+   * @returns
+   */
+  query: ({ sql, params = [], auto = true }) => {
+    if (auto) connect();
+    const asyncQuery = promisify(connection.query).bind(connection);
+    return asyncQuery(sql, params).then((result) => {
+      if (auto) end();
+      let data = null;
+      switch (sql.split(" ")[0]) {
+        case "SELECT":
+          data = result;
+          break;
+        case "INSERT":
+          data = result.insertId;
+          break;
+        case "UPDATE":
+          data = null;
+          break;
+        default:
+          data = null;
+          break;
+      }
+      return data;
+    });
+  },
+  connect,
+  end,
+};
