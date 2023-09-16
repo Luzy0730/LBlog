f21071963e83dc8045c5c5a28305b2517bd704c8
L.L <lu18958121319@sina.com>
2023-09-16 19:44:46 +0800

    fix: 修复404返回首页按钮失效

--- blog-cms/src/views/exception/404/index.vue
+++ blog-cms/src/views/exception/404/index.vue
@@ -1,13 +1,16 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+const router = useRouter()
+</script>
+
 <template>
-  <div
-    class="w-full h-full flex flex-col grow items-center justify-center py-3 gap-5"
-  >
+  <div class="w-full h-full flex flex-col grow items-center justify-center py-3 gap-5">
     <div class="w-1/3">
       <img src="/image/exception/404.svg" />
     </div>
     <div class="text-center text-slate-500 text-xl">
       <h1>抱歉，你访问的页面不存在</h1>
-      <el-button type="primary">回到首页</el-button>
+      <el-button type="primary" @click="router.push('/')">回到首页</el-button>
     </div>
   </div>
 </template>
