21c1a7b3f261df5f765781730bd5367470c3601f
L.L <lu18958121319@sina.com>
2023-09-16 19:46:21 +0800

    chore: 处理因1像素边框导致的滚动条

--- blog-cms/src/layouts/components/LayoutFooter.vue
+++ blog-cms/src/layouts/components/LayoutFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-full border-t-[1px] text-center leading-[60px]">
+  <div class="h-full text-center leading-[60px]">
     Copyright © 2023LL'S BLOG|浙ICP备2023006413号
   </div>
 </template>
