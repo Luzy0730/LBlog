fe871c3431a04dc0f1a75646fa06c4c19d30dace
陆震宇 <lu18958121319@sina.com>
2023-09-20 17:33:14 +0800

    chore: 更新轮播模块

--- blog-cms/src/components/content/config/Banner.vue
+++ blog-cms/src/components/content/config/Banner.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+const title = ref("LL's Blog")
+const bannerList = ref([{
+  id: 1,
+  url: 'https://lblog-aliyun.oss-cn-beijing.aliyuncs.com/bg1.jpg'
+}, {
+  id: 1,
+  url: 'https://lblog-aliyun.oss-cn-beijing.aliyuncs.com/bg2.png'
+}, {
+  id: 1,
+  url: 'https://lblog-aliyun.oss-cn-beijing.aliyuncs.com/bg3.jpg'
+}])
+</script>
+
 <template>
-  <div>首页轮播</div>
+  <el-card>
+    <el-input v-model="title" placeholder="标题" />
+  </el-card>
 </template>
