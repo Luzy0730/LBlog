7585c18b93d3294a3f268840854e48a25e157b0e
L.L <lu18958121319@sina.com>
2023-10-01 15:30:18 +0800

    fix: 修复异步获取站点信息导致的首次加载网站标题丢失问题

--- blog-web/src/permission.ts
+++ blog-web/src/permission.ts
@@ -2,14 +2,14 @@ import useTitle from '@/hooks/useTitle'
 import { storeToRefs } from 'pinia'
 import { useSystemStore, useUserStore } from "@/stores/index";
 import router from "@/router";
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   const systemStore = useSystemStore();
   const useStore = useUserStore();
-  if (!systemStore.siteInfo.blogName) {
-    systemStore.update_siteInfo()
-  }
   if (!useStore.introduction.name) {
     useStore.update_introduction()
+  }
+  if (!systemStore.siteInfo.blogName) {
+    await systemStore.update_siteInfo()
   }
   useTitle(to.meta.title as string)
   next()
