505cb29a3ce28a3215cf10031c675e553393a791
L.L <lu18958121319@sina.com>
2023-10-01 12:38:08 +0800

    feat: 添加路由跳转自动回顶

--- blog-web/src/router/index.ts
+++ blog-web/src/router/index.ts
@@ -3,6 +3,13 @@ import { BasicLayoutVue } from "@/layouts";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
   routes: [
     {
       path: "/",
