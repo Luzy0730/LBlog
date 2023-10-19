eef079555e51dcbec14967671fb3f64621a13c8d
L.L <lu18958121319@sina.com>
2023-10-19 21:55:24 +0800

    feat: 部署后台管理系统

--- blog-server/app/cms.js
+++ blog-server/app/cms.js
@@ -1,4 +1,4 @@
 const generateApp = require('./app')
 const { cmsServiceMount } = require("../services");
-const allowCORS = ['http://127.0.0.1:9999', 'http://localhost:9999']
+const allowCORS = ['http://www.lovcc.cn:7777', 'http://lovcc.cn:7777', 'http://127.0.0.1:9999', 'http://localhost:9999']
 generateApp(4000, cmsServiceMount, allowCORS)
